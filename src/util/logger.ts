0afdaff7a259405247d32904c030ed91f94d7b8d
davidgreenspan15 <ramigreenspangmail.com>
2022-08-21 23:15:32 -0400

    update logger one more time

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -3,18 +3,13 @@ import { createLogger, transports, format } from 'winston';
 const logger = createLogger({
   format: format.combine(
     format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss:ms' }),
-    format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`),
-    format.cli()
+    format.cli(),
+    format.printf(
+      info => `${info.level}: ${info.message.trim()} at ${info.timestamp} `
+    )
   ),
   level: 'silly',
-  transports: [
-    new transports.File({
-      filename: './logs/all-logs.log',
-      maxsize: 5242880,
-      maxFiles: 5,
-    }),
-    new transports.Console(),
-  ],
+  transports: [new transports.Console()],
 });
 
 export default logger;
